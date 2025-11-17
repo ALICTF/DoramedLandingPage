750a02acff2abb6ef529c73d3072c74a9c775b8e
ALICTF <alictf1@gmail.com>
2025-11-17 17:24:16 +0330

    add styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,22 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+            // ۱. اینجا فونت‌های قبلی شما هست
 			fontFamily: {
 				sans: ['Vazirmatn', 'sans-serif'],
 			},
+            // ۲. اینجا رنگ‌های جدید دورامد را اضافه کردیم
+            colors: {
+                doramed: {
+                    white: '#E0E1DD',    // رنگ متون روشن
+                    gray: '#778DA9',     // آبی خاکستری
+                    muted: '#415A77',    // آبی نفتی روشن
+                    primary: '#023E8A',  // آبی اصلی (دکمه‌ها)
+                    dark: '#1B263B',     // سرمه‌ای تیره (بخش‌ها)
+                    darker: '#0D1B2A',   // سرمه‌ای خیلی تیره (پس‌زمینه اصلی)
+                }
+            },
 		},
 	},
 	plugins: [],
 }
-
